1cf2e38799964702b85aa5d980450751b35fc121
Gayathri Subramanian <sgayathri88@gmail.com>
2019-06-22 17:04:25 -0400

    removed logging and merged with master

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-console.log(`databaseName is ${databaseName}`)
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
@@ -10,8 +9,6 @@ const db = new Sequelize(
     logging: false
   }
 )
-console.log('db is')
-console.log(db)
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
